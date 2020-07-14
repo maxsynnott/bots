14e8e9e2067d6fd47a711ccb7ce60e3f88266ff4
maxsynnott <maxryansynnott@gmail.com>
2020-07-14 17:49:35 +0200

    random snake bot

---
+++ snake/random/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.post('/move', (req, res) => {
+  const move = ['u', 'r', 'd', 'l'][Math.floor(Math.random() * 4)]
+
+  res.json({
+  	move: move
+  })
+})
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err)
+})
+
+// Error handler
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: req.app.get('env') === 'development' ? err : {}
+	})
+})
+
+const port = process.env.PORT || 2687
+
+app.listen(port, () => {
+  console.log('Server listening at port ' + port)
+})
